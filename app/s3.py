55b713585813af0d65889217a44a63e795a42dba
patrick muller <patrick42702@gmail.com>
2025-08-28 11:57:23 -0400

    added s3 module to connect to s3

--- app/s3.py
+++ app/s3.py
@@ -2,24 +2,16 @@ import os
 
 import boto3
 
-# Check environment
-ENV = os.environ.get("ENV", "development")  # default to "development"
-
 # Base session
 session = boto3.session.Session()
 
 # Common kwargs
 s3_kwargs = {
     # "endpoint_url": os.environ.get("S3_ENDPOINT"),
+    "region_name": os.environ.get("S3_REGION", "us-east-1"),
     "aws_access_key_id": os.environ.get("S3_ACCESS_KEY"),
     "aws_secret_access_key": os.environ.get("S3_SECRET_KEY"),
 }
-
-# Add endpoint_url in development only
-if ENV == "development":
-    s3_kwargs["endpoint_url"] = os.environ.get("S3_ENDPOINT")
-else:
-    s3_kwargs["region_name"] = os.environ.get("S3_REGION", "us-east-1")
 
 # Create the client
 s3 = session.client("s3", **s3_kwargs)
